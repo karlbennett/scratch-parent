2415999985ba90ebccff4dfdd7a4060e1fb3875d
karlbennett <where_1ts@yahoo.com>
2014-11-02 11:54:58 +1300

    Moved the TestController class into the ScratchSpringBootServletTest.

--- scratch-spring-boot-parent/scratch-spring-boot-war-parent/scratch-spring-boot-servlet/src/test/java/scratch/ScratchSpringBootServletTest.java
+++ scratch-spring-boot-parent/scratch-spring-boot-war-parent/scratch-spring-boot-servlet/src/test/java/scratch/ScratchSpringBootServletTest.java
@@ -8,15 +8,18 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import static org.junit.Assert.assertEquals;
-import static scratch.TestConstroller.TEST_MESSAGE;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = ScratchSpringBootServlet.class)
 @WebAppConfiguration("classpath:")
 @IntegrationTest({"server.port=0", "management.port=0"})
 public class ScratchSpringBootServletTest {
+
+    public static final String TEST_MESSAGE = "This is the response from the test controller.";
 
     @Value("${local.server.port}")
     private int port;
@@ -26,5 +29,15 @@ public class ScratchSpringBootServletTest {
 
         assertEquals("the request gets a valid response.", TEST_MESSAGE,
                 new TestRestTemplate().getForObject(String.format("http://localhost:%d/test", port), String.class));
+    }
+
+    @RestController
+    public static class TestController {
+
+        @RequestMapping("/test")
+        public String test() {
+
+            return TEST_MESSAGE;
+        }
     }
 }
